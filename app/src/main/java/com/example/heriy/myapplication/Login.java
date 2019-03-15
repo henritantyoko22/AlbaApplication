fe0186058c774e14d47610630feadf518cfa39b9
henritantyoko <henritantyoko22@gmail.com>
2019-03-15 18:52:11 +0700

    Update

--- app/src/main/java/com/example/heriy/myapplication/Login.java
+++ app/src/main/java/com/example/heriy/myapplication/Login.java
@@ -1,13 +1,27 @@
 package com.example.heriy.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button tombolmasuk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tombolmasuk = (Button)findViewById(R.id.msk);
+        tombolmasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,MenuUtama.class);
+                startActivity(intent);
+            }
+        });
     }
 }
